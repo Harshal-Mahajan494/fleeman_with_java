a92d99a36d796a0409ac44bd79f8acf8ead9c42a
VishnuAM01 <vishnu.arakkal.13@gmail.com>
2025-08-01 23:25:13 +0530

    Create AddOnDetailsRepository.java
    AddOnDetailsRepository

---
+++ src/main/java/com/example/repository/AddOnDetailsRepository.java
@@ -0,0 +1,19 @@
+package com.example.fleetmanagement.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.fleetmanagement.entity.AddOnDetails;
+
+public interface AddOnDetailsRepository extends JpaRepository<AddOnDetails, Short> {
+	
+	/*
+	 * Optional<T> is:
+	 * A container that may or may not contain a non-null value.
+	 * A safe and clean way to handle possibly missing data.
+	 */
+	Optional<AddOnDetails> findByAddOnName(String addOnName);
+	
+	boolean existsByAddOnName(String addOnName);
+
+}
